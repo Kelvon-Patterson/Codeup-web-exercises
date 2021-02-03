4a119de8fbde24648e3e0927600f98e708ec0630
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-02-02 20:05:00 -0600

    feat external js exercises

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -1,5 +1,15 @@
 "use strict";
 console.log("Hello from an external JS file");
 
-name = "Kelvon";
+var name = "Kelvon";
 console.log("Hello, + name");
+
+//Alert
+alert("Welcome to my site!");
+
+//Confirm
+var confirmLeave = confirm("Are you sure you want to leave? 0.0")
+console.log("confirmLeave" + confirmLeave )
+//Prompt
+var favoriteColor = prompt("Your favorite color is" )
+console.log("Your favorite color is" + prompt)
