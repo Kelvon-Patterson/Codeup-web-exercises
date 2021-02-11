75ef6d5708c1ec54348eba266f43ca3eee18a0ac
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-02-11 17:45:11 -0600

    feat For loop exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,33 +1,27 @@
-function showMultiplicationTable(num){
+// function showMultiplicationTable(num){
+//
+//     for (var i = 1 ; i <= 10 ; i++ ){
+// console.log(num + " x " + i + " = " + (num * i));
+//     }
+// }
+// showMultiplicationTable(100)
+//
+// // var random = Math.floor((Math.random() * 200) + 1);
+//
+// for( var i = 1 ; i <=10 ; i++){
+//     var random = Math.floor((Math.random() * 200) + 20);
+//     if(random % 2 === 0){
+//         console.log(random + " is even")
+//     }else{
+//         console.log( random  + " is odd ")
+//     }
+// }
 
-    for (var i = 1 ; i <= 10 ; i++ ){
-console.log(num + " x " + i + " = " + (num * i));
-    }
-}
-showMultiplicationTable(100)
-
-// var random = Math.floor((Math.random() * 200) + 1);
-
-for( var i = 1 ; i <=10 ; i++){
-    var random = Math.floor((Math.random() * 200) + 20);
-    if(random % 2 === 0){
-        console.log(random + " is even")
-    }else{
-        console.log( random  + " is odd ")
-    }
-}
-
-function generatePyramid() {
-    var totalNumberofRows = 5;
+for (var i = 1 ; i <= 9; i++){
     var output = '';
-    for (var i = 1; i <= totalNumberofRows; i++) {
-        for (var j = 1; j <= i; j++) {
-            output += j + '  ';
-        }
-        console.log(output);
-        output = '';
+    for(var j = 1; j <= i ; j++ ){
+        output += i.toString()
     }
+    console.log(output)
 }
-
-generatePyramid();
 
