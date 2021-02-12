6e13dd4f582d16f45c5d19e01c844087887ab1ee
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-02-12 16:46:04 -0600

    feat: change in #1 on for loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,33 +1,33 @@
-// function showMultiplicationTable(num){
-//
-//     for (var i = 1 ; i <= 10 ; i++ ){
-// console.log(num + " x " + i + " = " + (num * i));
-//     }
-// }
-// showMultiplicationTable(100)
-//
-// // var random = Math.floor((Math.random() * 200) + 1);
-//
-// for( var i = 1 ; i <=10 ; i++){
-//     var random = Math.floor((Math.random() * 200) + 20);
-//     if(random % 2 === 0){
-//         console.log(random + " is even")
-//     }else{
-//         console.log( random  + " is odd ")
-//     }
-// }
+function showMultiplicationTable(num){
 
-// for (var i = 1 ; i <= 9; i++){
-//     var output = '';
-//     for(var j = 1; j <= i ; j++ ){
-//         output += i.toString()
-//     }
-//     console.log(output)
-// }
+    for (var i = 1 ; i <= 10 ; i++ ){
+console.log(num + " x " + i + " = " + (num * i));
+    }
+}
+showMultiplicationTable(7)
 
-// for ( var i = 100; i >=5; i -= 5 ){
-//     console.log(i)
-// }
+// var random = Math.floor((Math.random() * 200) + 1);
+
+for( var i = 1 ; i <=10 ; i++){
+    var random = Math.floor((Math.random() * 200) + 20);
+    if(random % 2 === 0){
+        console.log(random + " is even")
+    }else{
+        console.log( random  + " is odd ")
+    }
+}
+
+for (var i = 1 ; i <= 9; i++){
+    var output = '';
+    for(var j = 1; j <= i ; j++ ){
+        output += i.toString()
+    }
+    console.log(output)
+}
+
+for ( var i = 100; i >=5; i -= 5 ){
+    console.log(i)
+}
 
 
 
