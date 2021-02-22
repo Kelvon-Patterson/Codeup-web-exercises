824b50c9a4a5c19a419ac22d2dafbcc185ce6c2d
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-02-22 15:48:12 -0600

    feat: updated exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,37 +1,93 @@
 function showMultiplicationTable(num){
-
-    for (var i = 1 ; i <= 10 ; i++ ){
-console.log(num + " x " + i + " = " + (num * i));
+    for(var i = 1; i <= 10 ; i++){
+        console.log(num + " x " + i +  " = " + (i * num))
     }
 }
 showMultiplicationTable(7)
 
-// var random = Math.floor((Math.random() * 200) + 1);
 
-for( var i = 1 ; i <=10 ; i++){
-    var random = Math.floor((Math.random() * 200) + 20);
+
+for (var i = 1; i <= 10; i++){
+
+    var random = Math.floor(Math.random() * 200) + 20
+
     if(random % 2 === 0){
-        console.log(random + " is even")
-    }else{
-        console.log( random  + " is odd ")
+        console.log(random + " is even ")
+    }else {
+        console.log( random + " is odd")
     }
 }
-//Refactor into a function
-function leftPyramid(num) {
-    for (var i = 1; i <= num; i++) {
-        var output = '';
-        for (var j = 1; j <= i; j++) {
-            output += i.toString()
-        }
-        console.log(output)
-    }
-}
-leftPyramid(10)
 
-for ( var i = 100; i >=5; i -= 5 ){
+for(var i = 1; i <= 9; i++){
+    output = ''
+    for(var j = 1; j <= i; j++){
+        output+= i.toString()
+    }
+   console.log(output)
+}
+
+for (var i = 100; i >= 5 ; i-=5 ){
+console.log(i)
+}
+
+
+var userNum= Number(prompt("Give me a number between 1 & 50"))
+
+function isNumberInRange(num){
+    return num>= 1 && num <= 50
+}
+function isNumberEven(num){
+    return num % 2 ===0
+}
+
+while (true) {
+
+    if (!isNumberEven(userNum) && isNumberInRange(userNum)) {
+        break;
+    }
+    userNum = Number(prompt("Give me a number between 1 & 50"));
+}
+    console.log("Number to skip is : " + userNum)
+
+for(var i = 1 ; i <=49 ; i += 2){
+    if(i === userNum) {
+        console.log('This is the number were skipping:' + i)
+    continue;
+    }
+    console.log("Here is a number were skipping " + i)
+}
+
+
+
+var i = 1
+
+while(i < 65536){
+    i*=2
     console.log(i)
+
 }
 
+var allCones = Math.floor(Math.random() * 50) + 50;
 
 
+console.log('The number of cones to sell is ' + allCones)
+
+do {
+    var someCones =Math.floor(Math.random() * 5) + 1;
+
+    if(someCones <= allCones){
+
+        allCones-= someCones
+
+        console.log('Cones sold ' + someCones + 'cones, and have ' + allCones + " left" )
+    }else{
+         console.log("Sorry I dont have " + someCones +" I only have " + allCones +" left")
+    }
+
+    i++
+}
+while(allCones>0)
+if(allCones === 0){
+    console.log("Yay we sold all the cones and can go home now!")
+}
 
--- w3resource_drills.js
+++ w3resource_drills.js
@@ -1,103 +1,137 @@
-function largerNum(num1, num2){
-    if(num1 > num2){
-        return num1
-    }else if(num2 > num1){
-        return num2
-    }else{
-        return NaN
-    }
-}
-//
-// function findSign(num1, num2, num3){
-//     if(){
-//
+// function largerNum(num1, num2){
+//     if(num1 > num2){
+//         return num1
+//     }else if(num2 > num1){
+//         return num2
+//     }else{
+//         return NaN
 //     }
 // }
-// dont forget to include zero
-for (var i = 1; i <=15 ; i++ ){
-    if(i % 2 ===0){
-        console.log(i + " is even")
-    }else if(i === 0){
-        console.log(i + " is even")
-    } else {
-        console.log(i + " is odd")
+// //
+// // function findSign(num1, num2, num3){
+// //     if(){
+// //
+// //     }
+// // }
+// // dont forget to include zero
+// for (var i = 1; i <=15 ; i++ ){
+//     if(i % 2 ===0){
+//         console.log(i + " is even")
+//     }else if(i === 0){
+//         console.log(i + " is even")
+//     } else {
+//         console.log(i + " is odd")
+//     }
+// }
+// var membersOfMyFamily = ['Kelvon' , 'Amirah', 'Rambo', 'Loki']
+// for ( var i = 0; i < membersOfMyFamily.length; i++){
+// console.log("The members of my family are " + membersOfMyFamily[i])
+// }
+//
+// ///While loop
+//
+// var i = 0
+//
+// while(i < 10){
+//     console.log("while loop iteration " +i);
+//     i++;
+// }
+//
+// var i = 10
+//
+// do{
+//     console.log("while loop iteration" + i );
+//     i++;
+// } while(i<10)
+//
+// //Exercise
+// var i = 1;
+//
+// while (i <65536){
+//
+//     console.log(i)
+// i+=i
+// }
+//
+// //For loop--- Most commonly used loops
+//
+// //for (//initialize (var i = 1); condition(i < 10); increment(i++)){};
+//
+// for ( var i = 0; i < 10; i++){
+//     console.log('for loop iteration' + i)
+//
+// }
+//
+// //convert to while
+// var i = 0
+// while(i < 10){
+//     console.log('for loop iteration');
+//     i++;
+// }
+//
+// //Exercise
+//
+// for ( var i = 100;i >= 5 ; i-=5 ){
+//
+// console.log(i)
+// }
+//
+// //Break and Continue
+//      var i = 0
+// while (i<10){
+//     console.log(i);
+//     if(i==5){
+//         break;
+//     }
+//     i++
+// }
+//
+// var i = 0;
+//
+// while(i <= 10){
+//     if(i==5){
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++
+// }
+//
+// function outputRectangle(width, height){
+//     for (var y = 1 ; y <= height; y++){
+//         var output= '';
+//
+//         for(var x = 1 ; x <= width ; x++){
+//             output+= "2"
+//         }
+//         console.log(output)
+//     }
+// }
+//
+// outputRectangle(4,6)
+//
+//
+// function multiplyTable(num) {
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + " X " + num + '='  + (i * num))
+//     }
+// }
+// multiplyTable(90)
+
+function listSet(NumberOfSets) {
+
+    for (var setNum = 1; setNum <= NumberOfSets; setNum++) {
+        console.log(" Set " + setNum)
     }
-}
-var membersOfMyFamily = ['Kelvon' , 'Amirah', 'Rambo', 'Loki']
-for ( var i = 0; i < membersOfMyFamily.length; i++){
-console.log("The members of my family are " + membersOfMyFamily[i])
-}
-
-///While loop
-
-var i = 0
-
-while(i < 10){
-    console.log("while loop iteration " +i);
-    i++;
-}
-
-var i = 10
-
-do{
-    console.log("while loop iteration" + i );
-    i++;
-} while(i<10)
-
-//Exercise
-var i = 1;
-
-while (i <65536){
-
-    console.log(i)
-i+=i
-}
-
-//For loop--- Most commonly used loops
-
-//for (//initialize (var i = 1); condition(i < 10); increment(i++)){};
-
-for ( var i = 0; i < 10; i++){
-    console.log('for loop iteration' + i)
 
 }
 
-//convert to while
-var i = 0
-while(i < 10){
-    console.log('for loop iteration');
-    i++;
+for (var exerciseNum = 1; exerciseNum <= 4; exerciseNum++) {
+    console.log(" Exercise " + exerciseNum + ' : ')
+    listSet(3)
 }
 
-//Exercise
-
-for ( var i = 100;i >= 5 ; i-=5 ){
-
-console.log(i)
-}
-
-//Break and Continue
-     var i = 0
-while (i<10){
-    console.log(i);
-    if(i==5){
-        break;
-    }
-    i++
-}
-
-var i = 0;
-
-while(i<10){
-    if(i==5){
-        i++;
-        continue;
-    }
-    console.log(i);
-    i++
-}
-
-
-
-//Create function that take a number 'n' and returns the first 10 multiples of 'n' with 1 added to it, seperate by commmas
-
+function exerciseTime(numOfExercises, numOfReps){
+    for (var exerciseNum = 1; exerciseNum <= numOfExercises; exerciseNum++) {
+        console.log(" Exercise " + exerciseNum + ' : ')
+}}
