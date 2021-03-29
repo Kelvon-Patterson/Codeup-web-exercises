af7a14325675cf24d85c5b038b6004d86620219d
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-03-29 10:43:06 -0500

    feat: beginning dom location exercises

---
+++ DOM-lec.js
@@ -0,0 +1,1 @@
+var topHeaderElement = document.getElementById("top-header");
--- DOM-student.js
+++ DOM-student.js
@@ -1,23 +1,44 @@
 //i. locating in the DOM
 
 // TODO: Find an element by the id of "hat-search". Store the result into a variable. Use a console log to check what is in your variable.
+var hatSearch = document.getElementById("hat-search");
+
+console.log(hatSearch)
 
 // TODO: Find all of our elements we made flex containers in Bootstrap! Locate the .d-flex class, store all of your results into a variable, and then console.log them.
+var flexContainers = document.getElementsByClassName("d-flex");
+
+console.log(flexContainers)
 
 // TODO: Finish my FOR loop to go through that variable containing our d-flex'ed items and console.log each individual one within the loop!
-
+for(var i=0; i < flexContainers.length; i++){
+    console.log(flexContainers[i])
+}
 // for(var ; i < .length; ++){
 // .. need to console.log some stuff!}
 
 // TODO: Find all of our div elements and store them into a variable~! Console.log the variable to see the results within!
+var divElements = document.getElementsByTagName("div");
+for(var i = 0; i < divElements.length; i++){
+    console.log(divElements);
+}
 
 // TODO: Now that we have a variable with all of the elements, can you tell me how many divs are in the HTML using that variable and a little Javascript? Hint: the variable looks and behaves a lot like an array, doesn't it? That helped us in our for loop's condition!
-
+//15 elements
 
 // TODO BONUS (a): Write a function called locateDivs. When locateDivs is called, it should return a collection of the div elements found.
-
+function locateDivs(){
+    return divElements
+}
+console.log(locateDivs())
 // TODO BONUS (b): Write a function called locateElements. locateElements should take in one parameter, the element to be searched for, and then return a collection of those elements. If no elements by that name were found, return false.
-
+function locateElements(element){
+    var element
+    if( element === true){
+         return document.getElementsByTagName("");
+         console.log()
+    }
+}
 // ii. a step further: accessing / looking into what we've located deeper
 
 // innerHTML/innerText
