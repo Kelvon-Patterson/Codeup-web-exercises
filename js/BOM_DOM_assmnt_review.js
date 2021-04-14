6d3682ecb1bcd2a7f6bceceb8bce27849699dc42
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-04-14 14:45:52 -0500

    feat: map manipulation and review of DOM/BOM

--- js/BOM_DOM_assmnt_review.js
+++ js/BOM_DOM_assmnt_review.js
@@ -1,22 +1,30 @@
 //TODO: Let's make a button- when the buttom is clicked the box should double in size from what it was
-
-$('#changeBoxSize').click(function(e){
-//stuff to have happen when click happens- on the id of changeBoxSize
-    $('#box').css('height', '500px');
-
-    $('#box').css('width', '500px');
+$('#changeBoxSize').click(function (e){
+    $('#box').css('width','500px')
+    $('#box').css('height', '500px')
 })
+//stuff to have happen when click happens- on the id of changeBoxSize
 
-//TODO:Let's make a class - when the toggle class button- when the toggle class button is clicked, the boc will change color to the class's background -color
 
-var instructions = function (e){
-    if($("#box").hasClass("toggleClass1")){
-        $("#box").toggleClass("toggleClass2")
-    } else {
-        $("#box").toggleClass("toggleClass1")
+//TODO:Let's make a class - when the toggle class button- when the toggle class button is clicked, the box will change color to the class's background -color
+var instructions = function(e){
+    if($('#box').hasClass("toggleClass1")) {
+        $('#box').toggleClass('toggleClass2')
+    }
+    else{
+        $('#box').toggleClass('toggleClass1')
     }
 }
+    $('#toggleClass').click(instructions)
 
-$("#toggleClass").click(instructions)
+// var instructions = function (e){
+//     if($("#box").hasClass("toggleClass1")){
+//         $("#box").toggleClass("toggleClass2")
+//     } else {
+//         $("#box").toggleClass("toggleClass1")
+//     }
+// }
+// $("#toggleClass").click(instructions)
+
 //TODO: can we set up an area of HTML - in that area of HTML, when the BOX is hovered over you would see the height and width of the BOX
 
--- js/weather_map.js
+++ js/weather_map.js
@@ -17,16 +17,14 @@ mapboxgl.accessToken = mapBoxToken;
             }).done(function (results){
                 // console.log(results.daily[0].temp.day)
                 var html = '';
-                for (var i= 0; i < 7; i++){
+                for (var i= 0; i < 5; i++){
                     // html +=  renderWeather(results.daily[i])
-                    html ='<div>'
-                    html += '<div>';
-                    html += '<h3>'+ 'Date: ' + new Date(results.daily[i].dt*1000).toDateString() +'</h3>';
+                    html ='<div class="card">'
+                    html += '<h3>'+ new Date(results.daily[i].dt*1000).toDateString() +'</h3>';
                     html += '<p>' + 'Temperature: ' +Math.floor( results.daily[i].temp.min) + '/' + Math.floor( results.daily[i].temp.max) +'</p>';
                     html += '<p>' + 'Description: '+results.daily[i].weather[0].description  + '</p>';
                     html += '<p>' +'Pressure: ' +results.daily[i].pressure + '</p>';
                     html += '<p>' + 'Wind Speed: ' +results.daily[i].wind_speed + '</p>';
-                    html += '</div>';
                     html+= '</div>'
                     $('#forecast').append(html)
                 }
@@ -36,56 +34,4 @@ mapboxgl.accessToken = mapBoxToken;
 
             renderWeather()
 
-                    // html +=  renderWeather(results.daily[i])
-                    // results.daily.forEach(function(day,index){
-                    //
-                    // $('.date').html( 'Date: ' + new Date(day.dt*1000).toDateString());
-                    // $('.temp').html( 'Temperature: ' +day.temp.min + '/' + day.temp.max) ;
-                    // $('.description').html(  'Description:'+ day.weather[0].description );
-                    // $('.pressure').html( 'Pressure: '+ day.pressure);
-                    // $('.windSpeed').html( 'Wind Speed: ' +day.wind_speed);
-
-// })
-    //
-    //                 $('.render-weather').html(html)
-    //
-    //
-    //             }
-    //
-    //         })
-    //         return html
-    //     }
-
-//0
-// function onDrangEnd(){
-//             var longlat=marker.getLngLat();
-//             longlat=[longlar.lng], longlat.lat
-//
-// }
-// var weatherDiv= document.querySelector('');
-// weatherDiv.innerHTML= renderWeather(weatherRequest)
-
-
-    //     "dt": 1618289204,
-    //     "sunrise": 1618229421,
-    //     "sunset": 1618275507,
-    //     "temp": 73.78,
-    //     "feels_like": 74.28,
-    //     "pressure": 1011,
-    //     "humidity": 73,
-    //     "dew_point": 64.58,
-    //     "uvi": 0,
-    //     "clouds": 1,
-    //     "visibility": 10000,
-    //     "wind_speed": 11.5,
-    //     "wind_deg": 90,
-    //     "weather": [
-    //     {
-    //         "id": 800,
-    //         "main": "Clear",
-    //         "description": "clear sky",
-    //         "icon": "01n"
-    //     }
-    // ]
-    // }
 
