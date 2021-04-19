467c81926c529323ede22a1e553479e1641d88c7
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-04-19 16:23:32 -0500

    feat: restful Promises lecture

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,14 @@
-function getGithubUsername(username){
-    let promise = fetch(`https://api.github.com/users/${username}/events`, {headers:{'Authorization': `token ${githubAPIKey}`}})
+function getLastPushDate(githubUsername){
 
-    promise.then(response => response.json()).then(events=>console.log(events))
+    let promise = fetch(`https://api.github.com/users/${githubUsername}/events`, {headers:{'Authorization': `token ${githubAPIKey}`}})
+
+    promise.then(response => response.json()).then(events=>{ for(let event of events){
+        if(event.type === 'PushEvent'){
+            console.log(event.created_at)
+            return new Date(event.created_at)
+        }
+    }
+    })
 }
-console.log(getGithubUsername())
+console.log(getLastPushDate('Kelvon-Patterson'))
+    .then((date)=>console.log(date));
